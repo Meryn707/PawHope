2335fa5f1a1c9a869ca202525c16b9f9f7556193
Mayra <mayra.mancebo@alten.es>
2025-05-25 20:44:58 +0200

    comienzo del proyecto

---
+++ src/main/java/com/tfg/pawhope/PawHopeApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.pawhope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawHopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawHopeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tfg/pawhope/PawHopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.pawhope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawHopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
